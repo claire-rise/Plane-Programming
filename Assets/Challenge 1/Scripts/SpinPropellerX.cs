bbf334de336cc241062037d5dac0ebc4732e4313
ofoetan olusola temitayo <63857325+claire-rise@users.noreply.github.com>
2022-09-12 02:12:09 +0100

    Initial commit

---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpinPropellerX : MonoBehaviour
+{
+    
+    //Create our propellerRotationSpeed variable
+    private float rotationSpeed = 100f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        // spin the propeller
+        transform.Rotate(Vector3.forward * rotationSpeed);
+    }
+}
